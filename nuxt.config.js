1d6c30f3d6b33700d7f9c5a46abdf7863d7384bd
Toc <tocwebtoc@gmail.com>
2021-04-25 11:05:10 +0200

    Setup vue-ellipse-progress plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,6 +97,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-ellipse-progress.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
