97a5f2697b3f372b3833e69dd424d3e05e7e52ae
Toc <tocwebtoc@gmail.com>
2021-04-26 10:41:42 +0200

    Correction of typo text mistakes in mentionslegales.vue

--- pages/mentionslegales.vue
+++ pages/mentionslegales.vue
@@ -1,69 +1,69 @@
 <template>
     <div class="relative bg-white overflow-hidden pb-10">
         <h1 class="kf-moveInDown text-3xl text-center tracking-tight font-extrabold text-gray-900 pt-5 sm:pt-14 md:text-4xl">Mentions Légales</h1>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Editeur</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Editeur</h2>
         <hr class="hr-mentions w-20 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Ce site est édité par l'Université  de Lille<br>
             42 rue Paul Duez 59000 Lille<br>
             Forme juridique : établissement public à caractère scientifique, culturel et professionnel<br>
-            Adresse électronique : contact@univ-lillefr<br>
+            Adresse électronique : contact@univ-lille.fr<br>
             Tél. :  +33 (0)3 20 96 43 43<br>
             Fax : +33 (0)3 20 88 24 32
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Directeur de la Publication</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Directeur de la Publication</h2>
         <hr class="hr-mentions w-72 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Monsieur Jean-Christophe Camart, Président de l'Université de Lille
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Hébergement du site</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Hébergement du site</h2>
         <hr class="hr-mentions w-56 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Centre de Ressources Informatiques - Université de Lille<br>
-            Domaine universitaire du "Pont de Bois rue du Barreau<br>
+            Domaine universitaire du Pont de Bois rue du Barreau<br>
             BP 60149, 59653 Lille
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Conception Web</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Conception Web</h2>
         <hr class="hr-mentions w-64 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Ass Ouest Wax Création
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Technologies utilisées</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Technologies utilisées</h2>
         <hr class="hr-mentions w-60 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Nuxt.js 
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Ligne éditoriale</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Ligne éditoriale</h2>
         <hr class="hr-mentions w-44 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-center pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-center pt-3">
             Direction de l'innovation pédagogique de l'Université de Lille
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Droits d'auteur et propriété intellectuelle</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Droits d'auteur et propriété intellectuelle</h2>
         <hr class="hr-mentions w-72 sm:w-96 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto pt-3 text-justify">
+        <p class="w-11/12 sm:w-2/3 mx-auto pt-3 text-justify">
             L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés.
             La reproduction de tout ou partie de ce site sur un support électronique quel qu'il soit est formellement interdite sauf autorisation expresse du directeur de la publication.
-            Les photographies, textes, dessins, images etc. protégés par les droits de la propriété intellectuelle sont la propriété de L'Université de Lille ou de tiers ayant permis à l'Université de Lille d'en faire usage.
+            Les photographies, textes, dessins, images etc. protégés par les droits de la propriété intellectuelle sont la propriété de l'Université de Lille ou de tiers ayant permis à l'Université de Lille d'en faire usage.
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Avertissement concernant les informations disponibles sur ce site</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Avertissement concernant les informations disponibles sur ce site</h2>
         <hr class="hr-mentions w-72 sm:w-96 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-justify pt-3">
-            L'Université de Lille s'efforce d'assurer au mieux de ses possibilités, l'exactitude et la mise à jour des informations diffusées sur ce site, dont elle se réserve le droit de corriger, à tout moment et sans préavis, le contenu. Toutefois, L'Université de Lille ne peut garantir l'exactitude, la précision ou l'exhaustivité des informations mises à la disposition sur ce site. En conséquence, L'Université de Lille décline toute responsabilité : pour toute interruption du site
+        <p class="w-11/12 sm:w-2/3 mx-auto text-justify pt-3">
+            L'Université de Lille s'efforce d'assurer au mieux de ses possibilités, l'exactitude et la mise à jour des informations diffusées sur ce site, dont elle se réserve le droit de corriger, à tout moment et sans préavis, le contenu. Toutefois, l'Université de Lille ne peut garantir l'exactitude, la précision ou l'exhaustivité des informations mises à la disposition sur ce site. En conséquence, l'Université de Lille décline toute responsabilité : pour toute interruption du site
             survenance de bogues; pour toute inexactitude ou omission portant sur des informations disponibles sur le site; pour tous dommages résultant d'une intrusion frauduleuse d'un tiers ayant entraîné une modification des informations mises à la disposition sur le site.
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Liens hypertextes</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Liens hypertextes</h2>
         <hr class="hr-mentions w-44 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-justify pt-3">
-            Le site peut inclure des liens vers d'autres sites Web ou d'autres sources Internet. Dans la mesure où l'Université de Lille ne peut contrôler ces sites et ces sources externes, elle ne peut être tenue pour responsable de la mise à disposition de ces sites et sources externes, et ne peut supporter aucune responsabilité quant au contenu, publicités, produits, services ou tout autre matériel disponible sur ou à partir de ces sites ou sources externes. De plus, l'Université de Lille ne pourra être tenu responsable de tous dommages ou pertes avérés ou allégués consécutifs ou en relation avec l'utilisation ou avec le fait d'avoir fait confiance au contenu, à des biens ou des services disponibles sur ces sites ou sources externes.
+        <p class="w-11/12 sm:w-2/3 mx-auto text-justify pt-3">
+            Le site peut inclure des liens vers d'autres sites Web ou d'autres sources Internet. Dans la mesure où l'Université de Lille ne peut contrôler ces sites et ces sources externes, elle ne peut être tenue pour responsable de la mise à disposition de ces sites et sources externes, et ne peut supporter aucune responsabilité quant au contenu, publicités, produits, services ou tout autre matériel disponible sur ou à partir de ces sites ou sources externes. De plus, l'Université de Lille ne pourra être tenue responsable de tous dommages ou pertes avérés ou allégués consécutifs ou en relation avec l'utilisation ou avec le fait d'avoir fait confiance au contenu, à des biens ou des services disponibles sur ces sites ou sources externes.
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Cookies / Témoins</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Cookies / Témoins</h2>
         <hr class="hr-mentions w-44 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-justify pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-justify pt-3">
             Les témoins servent à faciliter la navigation sur le site et à rationaliser les procédures d'enregistrement ou de mesure d'audience. Les témoins sont des fichiers texte placés sur le disque dur de votre ordinateur par un serveur de page Web. Votre ordinateur est sans doute configuré pour accepter les témoins. Vous pouvez cependant modifier ce paramètre pour qu'il les refuse et l'Université de Lille vous suggère de le faire si l'utilisation des témoins vous inquiète. Quand vous visitez ce site Web, pour chercher, lire ou télécharger de l'information, l'Université de Lille recueille et conserve certains "renseignements visiteur" vous concernant, tels le nom du domaine et de l'ordinateur hôte à partir desquels vous allez sur Internet, l'adresse du protocole Internet (IP) de l'ordinateur que vous utilisez, la date et l'heure de votre navigation sur ce site, et les URL à partir desquelles vous êtes passé à ce site. L'Université de Lille utilise cette information pour analyser et mesurer la fréquentation de son site et pour travailler à le rendre plus utile. L'Université de Lille détruit cette information après un certain temps. Si vous ne voulez pas que l'Université de Lille recueille ce genre d'information, ne naviguez pas sur son site.
         </p>
-        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-700 pt-5 sm:pt-8 md:text-3xl">Politique de confidentialité</h2>
+        <h2 class="kf-moveInDown text-2xl text-center tracking-tight font-extrabold text-pink-900 pt-5 sm:pt-8 md:text-3xl">Politique de confidentialité</h2>
         <hr class="hr-mentions w-64 mx-auto">
-        <p class="w-11/12 sm:w-1/2 mx-auto text-justify pt-3">
+        <p class="w-11/12 sm:w-2/3 mx-auto text-justify pt-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean est nibh, accumsan quis nibh vitae, dictum finibus lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse risus massa, consequat vel vestibulum et, efficitur luctus metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam pellentesque sed eros vitae sollicitudin. Quisque eget sagittis lorem. Suspendisse ante sem, semper id aliquam eget, ullamcorper egestas nunc.
             Fusce cursus est pulvinar, feugiat mauris et, feugiat ex. Integer suscipit ex sed ante tincidunt feugiat. Suspendisse lobortis feugiat arcu. Suspendisse ultrices lectus et ex ullamcorper.
         </p>
