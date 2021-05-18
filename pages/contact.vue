c799a73f5c8a1cedfd1266d482fcb1b777e33a2e
Toc <tocwebtoc@gmail.com>
2021-05-18 09:50:09 +0200

    README.md update / Replacement of <picture> by simple <img> in index.vue  & contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,11 +8,7 @@
                 <div class="lg:py-4 lg:pb-32 lg:flex lg:justify-center">
                     <div class="lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
                         <div class="bg-white lg:w-1/2 lg:p-5 ">
-                            <picture>
-                                <source media="(max-width: 575.97px)" srcset="~/assets/img/undraw_Work_time_re_hdyv.svg">
-                                <source media="(min-width: 575.98px)" srcset="~/assets/img/undraw_Work_time_re_hdyv.svg">
-                                <img class="kf-moveInLeft h-48 w-full sm:h-60 md:h-64 lg:h-full lg:rounded-lg" src="~/assets/img/undraw_Work_time_re_hdyv.svg" width="472" height="320" alt="illustration de deux personnes travaillant à une table commune">
-                            </picture> 
+                            <img class="kf-moveInLeft h-48 w-full sm:h-60 md:h-64 lg:h-full lg:rounded-lg" src="~/assets/img/undraw_Work_time_re_hdyv.svg" width="472" height="320" alt="illustration de deux personnes travaillant à une table commune">
                         </div>
                         <div class="bg-pink-50 flex flex-col justify-center items-center xl:items-left h-full bg-opacity-50 hover:bg-opacity-70 py-12 px-3 md:px-10 max-w-5xl lg:w-1/2">
                             <h2 class="lg:mt-5 text-2xl text-gray-800 font-bold">Partager votre<span class="text-pink-800"> expérience</span></h2>
